9f673bd3027c1f9de120ff57d96e2a9d854bb00c
AbahEkang <charles.abah@live.it>
2022-10-14 11:51:37 +0100

    Adding images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -31,7 +31,9 @@ func routes(app *Config.AppConfig) http.Handler{
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
-	
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
 
